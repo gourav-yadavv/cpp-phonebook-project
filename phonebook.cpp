e006e677f641a90be20a8c98354da6303a179640
Gourav Yadav <79785892+gourav-yadavv@users.noreply.github.com>
2023-07-10 18:13:59 +0530

    Update phonebook.cpp

--- phonebook.cpp
+++ phonebook.cpp
@@ -535,29 +535,3 @@ int main()
 
     return 0;
 }
-
-/*Both AVL trees and red-black trees are self-balancing binary search trees that provide efficient operations for maintaining a sorted order and searching. Here's a detailed comparison of these two data structures:
-
-Balance Factor:
-
-AVL trees use a balance factor, which is the difference between the heights of the left and right subtrees, to ensure the tree remains balanced. The balance factor of any node in an AVL tree must be either -1, 0, or 1.
-Red-black trees use color properties (red or black) on nodes to maintain balance. The color properties ensure that no path from the root to a leaf has more than twice the length of any other path.
-Balancing Mechanism:
-
-AVL trees perform more rotations than red-black trees to maintain balance. This results in faster search operations but slower insertion and deletion operations compared to red-black trees.
-Red-black trees have a simpler balancing mechanism that requires fewer rotations than AVL trees. This makes them slightly more efficient for insertion and deletion operations but can lead to slower search operations compared to AVL trees.
-Time Complexity:
-
-Both AVL trees and red-black trees provide efficient time complexities for operations:
-Search: O(log n)
-Insertion: O(log n)
-Deletion: O(log n)
-However, AVL trees typically have faster search times due to their more rigid balance criteria, which guarantees a maximum height difference of 1 between subtrees.
-Space Overhead:
-
-Red-black trees require additional space to store color information (typically one extra bit per node). This slightly increases the memory overhead compared to AVL trees.
-Use Cases:
-
-AVL trees are generally preferred when the application requires faster search times, such as in databases or applications with more frequent read operations.
-Red-black trees are often chosen when a good balance between search, insertion, and deletion operations is needed, such as in file systems or memory management.
-In summary, the choice between AVL trees and red-black trees depends on the specific requirements of your application. If you prioritize faster search times and can tolerate slightly slower insertion and deletion operations, AVL trees may be the better choice. On the other hand, if you seek a balance between all operations and prefer a simpler balancing mechanism, red-black trees are a good option. Both data structures offer efficient performance for small to moderate-sized phonebooks like the one you described with a maximum of 500 contacts.*/
